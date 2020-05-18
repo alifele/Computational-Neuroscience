980f028f3ec78fed29b906e282f8e4300153a8e5
alifele <ali.fele@gmail.com>
2020-05-18 22:40:27 +0430

    neurons model update

--- update.sh
+++ update.sh
@@ -1,9 +1,9 @@
 git add --a
 #echo "$1"
 git commit -a -m "$1"
-git push origin master
+echo "alifele" | git push origin master
 
-echo alifele
-echo 1362290696Afp
+#echo alifele
+#echo 1362290696Afp
 
 #echo $1
